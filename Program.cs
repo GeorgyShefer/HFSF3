786b2294fe264fbcf9ba01bf6895d68972d8add6
GeorgyShefer <132808044+GeorgyShefer@users.noreply.github.com>
2023-06-12 16:20:01 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            DirectoryInfo necessaryDelete = new DirectoryInfo("C:\\Users\\user\\Desktop\\FoldersForDelete");
+            string folder = "C:\\Users\\user\\Desktop\\FoldersForDelete";
+
+            Console.Write("Размер папки: ");
+            Console.WriteLine(folder.Length + " байт");
+            Console.Write("Очищено: ");
+            Console.WriteLine(folder.Length + " байт");
+            Console.WriteLine(TimeSpan.FromMinutes(30));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
